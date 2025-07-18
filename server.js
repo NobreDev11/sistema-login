c619bb3db177e30b29d259ebc4dd843fa4404b4d
Nobre_Dev <nobre.dev11@gmail.com>
2025-07-18 08:22:30 -0300

    corrige app.use

--- server.js
+++ server.js
@@ -3,17 +3,15 @@ const cors = require('cors');
 const dotenv = require('dotenv');
 const conectarBanco = require('./config/db');
 
+const allowedOrigins = [
+  'https://frontend-login-8dlb.onrender.com',
+];
+
 dotenv.config();
 conectarBanco();
 
 const app = express();
 
-// Lista de origens permitidas (frontend no Render)
-const allowedOrigins = [
-  'https://frontend-login-8dlb.onrender.com',
-];
-
-// Configura CORS com origem específica
 app.use(cors({
   origin: function (origin, callback) {
     if (!origin || allowedOrigins.includes(origin)) {
@@ -25,13 +23,11 @@ app.use(cors({
   credentials: true,
 }));
 
-// Middleware para ler JSON
 app.use(express.json());
 
-// Rotas da API
+// rotas
 app.use('/api/usuarios', require('./routes/usuariosRoutes'));
 
-// Inicializa servidor
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);

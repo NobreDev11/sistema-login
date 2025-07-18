63c00e8b909a6d19586f4b149b2d7990dc21b207
Nobre_Dev <nobre.dev11@gmail.com>
2025-07-17 21:53:35 -0300

    Corrigido CORS para aceitar frontend Render

--- server.js
+++ server.js
@@ -3,14 +3,25 @@ const cors = require('cors');
 const dotenv = require('dotenv');
 const conectarBanco = require('./config/db');
 
+const allowedOrigins = [
+  'https://frontend-login-8dlb.onrender.com', // seu frontend no Render
+];
+
 dotenv.config();
 conectarBanco();
 
 const app = express();
 
 // 👉 Libera o frontend específico:
-app.use(cors({
-  origin: 'https://frontend-login-8dlb.onrender.com'
+aapp.use(cors({
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true,
 }));
 
 app.use(express.json());
